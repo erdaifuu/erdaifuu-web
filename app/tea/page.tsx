f0fdc40928bbec2bbbaa7d36118d01113992b337
erdaifuu <erdaifuu@gmail.com>
2025-10-30 05:30:54 -0700

    Added images, and small changes

--- app/tea/page.tsx
+++ app/tea/page.tsx
@@ -2,7 +2,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Tea</h1>
-      Work in Progress
+      Work in Progress. Here's my pumidor though :3
+      <div>
+        <img src={'../images/general/pumidor.jpg'} alt="Pumidor" />
+      </div>
     </section>
   )
 }
